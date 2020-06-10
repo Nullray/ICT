65007363b89fe5a008d19fc80eb110145b37930d
Yisong Chang <changyisong@ict.ac.cn>
2020-06-10 22:25:33 +0800

    software: linux: Compilation of Linux Dom0 kernel

--- software/scripts/kernel.mk
+++ software/scripts/kernel.mk
@@ -7,4 +7,6 @@ KERN_TARGET := all
 # TODO: Change to your own Linux kernel configuration file name for physical os
 phy_os-kern-config:= xilinx_zynqmp_defconfig
 phy_os-kern-config-file := $(KERN_SRC)/arch/$(KERN_PLAT)/configs/$(phy_os-kern-config)
+dom0-kern-config:= xilinx_zynqmp_xen_Dom0_defconfig
+dom0-kern-config-file := $(KERN_SRC)/arch/$(KERN_PLAT)/configs/$(dom0-kern-config)
 
