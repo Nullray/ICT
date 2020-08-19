5f958bb66ae3404479e10e9085b7d9073f03492a
Yisong Chang <changyisong@ict.ac.cn>
2020-08-19 21:25:20 +0800

    software: Fix directory paths of files generated from U-Boot compilation

--- software/scripts/uboot.mk
+++ software/scripts/uboot.mk
@@ -1,7 +1,7 @@
 # TODO: set your U-Boot compilation flags and targets
 UBOOT_CROSS_COMPILE_FLAGS := ARCH=arm \
 	CROSS_COMPILE=aarch64-linux-gnu- \
-	EXT_DTB=$(UBOOT_SRC)/../../ready_for_download/$(DTB_LOC)/system.dtb
+	EXT_DTB=$(UBOOT_LOC)/../../ready_for_download/$(DTB_LOC)/system.dtb
 UBOOT_TARGET := all
 
 # TODO: Change to your own U-Boot configuration file name
