3f4bd99fd8af2225c0e072d57e984029ca318463
Yisong Chang <changyisong@ict.ac.cn>
2022-02-09 15:41:12 +0800

    workload: ucas-cod: add dma_test in simple_test benchmark suite

---
+++ software/workload/ucas-cod/benchmark/simple_test/dma_test/include/perf_cnt.h
@@ -0,0 +1,17 @@
+
+#ifndef __PERF_CNT__
+#define __PERF_CNT__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct Result {
+  unsigned long msec;
+} Result;
+
+//unsigned long _uptime();
+void bench_prepare(Result *res);
+void bench_done(Result *res);
+
+#endif
