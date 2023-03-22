69647b6e0bdfc2a79c222487e52296245fc5ff5f
Nullray <2421709399@qq.com>
2023-03-22 16:36:28 +0800

    nf.dtsi changed

---
+++ submodule.sh
@@ -0,0 +1,18 @@
+ #!/bin/bash
+ #git clone -b xilinx-v2019.1-nf-dev git@github.com:ict-accel-team/arm-trusted-firmware.git software/atf
+ #git clone -b xilinx-v2019.1-nf-dev git@github.com:ict-accel-team/device-tree-xlnx.git bootstrap/dt-gen
+ #git clone -b xilinx-v2019.1-nf-dev git@github.com:ict-accel-team/xilinx-xen.git software/xen
+ #git clone -b v1.20.1-dev git@github.com:ict-accel-team/ipxe.git bootstrap/ipxe
+ #git clone -b xilinx-v2020.1-dev git@github.com:ict-accel-team/linux-xlnx.git software/linux
+ #git clone -b tmp git@github.com:ict-accel-team/openbmc.git software/openbmc
+ #git clone -b xilinx-v2020.1-dev git@github.com:ict-accel-team/u-boot-xlnx.git software/uboot
+ git clone git@github.com:ict-accel-team/opensbi.git software/opensbi/src
+ git clone git@github.com:ict-accel-team/sm.git software/opensbi/sm
+ git clone git@github.com:ict-accel-team/SERVE.git fpga/design/serve
+ git clone git@github.com:ict-accel-team/linux-keystone-driver.git software/modules/keystone-driver
+ git clone git@github.com:ict-accel-team/keystone-sdk.git software/workload/keystone/sdk
+ git clone git@github.com:ict-accel-team/keystone-runtime.git software/workload/keystone/eyrie
+ git clone git@github.com:ict-accel-team/nf-accel-shell.git fpga/design/nf-accel-shell
+ git clone -b 3.5.0-dev git@github.com:ict-accel-team/optee_os.git software/tee/optee
+ # git clone git@github.com:ict-accel-team/nf-firesim.git fpga/design/nf-firesim
+ # git clone git@github.com:ict-accel-team/ucas-cod.git fpga/design/ucas-cod
